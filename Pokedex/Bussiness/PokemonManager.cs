3f91836dec74d83975c04340f38ada9d9088256e
sergio <jarasergio@outlook.com>
2016-10-07 21:13:09 -0300

    initial commit

---
+++ Pokedex/Bussiness/PokemonManager.cs
@@ -0,0 +1,28 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+
+namespace Bussiness
+{
+    public class PokemonManager
+    {
+        public void RegisterPokemon(string name, string trainer)
+        {
+            var pok = new Pokemon(name, trainer);
+            var datamanager = new DataManager();
+            datamanager.AddPokemon(pok);
+
+        }
+
+        public List<Pokemon> GetRegisteredPokemon()
+        {
+            var datamanager = new DataManager();
+            var pokemons = datamanager.GetPokemons();
+            return pokemons;
+        }
+    }
+}
---
+++ Pokedex/Data/Pokemon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Data.Models
+{
+    public class Pokemon
+    {
+
+        public string Name { get; set; }
+          
+        public string Trainer { get; set; }
+
+        public Pokemon(string name, string trainer) {
+            this.Name = name;
+            this.Trainer = trainer;            
+        }
+
+
+
+      
+    }
+}
